25e3dcf506fa43b6408963b23a6781ecb196527c
22120231 <22120231@student.hcmus.edu.vn>
2025-04-20 18:32:53 +0700

    App Initialization

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config(); 
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// TN: Dùng thư viện cors để cho phép kết nối http từ client đến server
+app.use(cors( 
+    {
+        origin: process.env.WEB_URL,
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    }
+));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT} at http://localhost:${PORT}`);
+});
+
