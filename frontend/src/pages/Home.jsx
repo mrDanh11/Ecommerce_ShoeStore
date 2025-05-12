cd4898f389df49a82ed37ec230ef5c5b1b03c1bc
qdawwn <thnndat236@gmail.com>
2025-05-12 09:06:09 +0700

    Test branch datle

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <>
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-            <h1 className="text-4xl font-bold mb-4">Welcome to {INFO.storeName} Website, {INFO.username}!</h1>
+            <h1 className="text-4xl font-bold mb-4">Welcome to {INFO.storeName} Website, {INFO.username}! Hello world!</h1>
         </div>
     </>
   )
