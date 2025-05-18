5c2a2dea7ce56d66007bb57ea6e6f9ca49938cc1
NGLong <101536564+Aesop13@users.noreply.github.com>
2025-05-19 01:16:34 +0700

    Update authRoutes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config(); 
 const express = require('express');
 const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(cors(
 ));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT;
 
