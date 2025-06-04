71ded74d5a52920fb2de960482f83b22c24465fd
Mr Danh <danhnguyen01112@gmail.com>
2025-06-04 10:07:35 +0700

    FE: add footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar"; // Thêm dòng này
 import Topbar from "./components/Topbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </>
   )
 };
