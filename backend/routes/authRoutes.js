da9a412351c50f1a0ced63dc68d3407facb21a09
NGLong <101536564+Aesop13@users.noreply.github.com>
2025-06-12 16:45:06 +0700

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 // Đăng ký tài khoản
 router.post('/register', authController.register);
