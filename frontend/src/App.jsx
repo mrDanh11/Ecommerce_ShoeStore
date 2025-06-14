5e904e29b0e3c80ea3c003cb73be9a2d624ec4eb
NGLong <101536564+Aesop13@users.noreply.github.com>
2025-06-15 05:18:20 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,25 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Navbar from "./components/Navbar"; // Thêm dòng này
+import Navbar from "./components/Navbar";
 import CartPage from "./pages/CartPage";
+import Login from "./pages/authentication/Login";
+import Register from "./pages/authentication/Register";
+import ForgotPassword from "./pages/authentication/ForgotPassword";
+import CallBack from "./pages/authentication/CallBack";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
     <>
-      {/* <Topbar /> */}
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/callback" element={<CallBack />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </>
   )
